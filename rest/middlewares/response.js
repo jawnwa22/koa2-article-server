468765558e85414341a3a7be0227d513e1973e61
dragon <dragon@dragondeMacBook.lan>
2019-03-08 12:46:15 +0800

    新增错误打印到终端功能

--- rest/middlewares/response.js
+++ rest/middlewares/response.js
@@ -4,12 +4,14 @@
  * @error status 响应错误返回的错误码
  * @error msg 自定义的错误提示信息 
  * @success msg 自定义的返回提示信息
+ * @success data 请求成功返回的数据
  * 
  */
 
 export default async (ctx, next) => {
   ctx.error = ({ status, msg }) => {
     ctx.status = status || 400;
+    console.log(msg);
     ctx.body = { code: 'error', msg };
   }
   ctx.success = ({ msg, data }) => {
