307c548d96d26a583a3c80ff7de2e25dc10c9b85
dragon <dragon@dragondeMacBook.lan>
2019-03-08 12:46:35 +0800

    新增用户model

--- rest/models/index.js
+++ rest/models/index.js
@@ -1,10 +1,12 @@
 import ArticleModel from './article/article.model';
 import CategoryModel from "./article/category.model";
 import TagModel from './article/tag.model';
+import UserModel from './article/user.model';
 
 
 export {
   ArticleModel,
   CategoryModel,
-  TagModel
+  TagModel,
+  UserModel
 }
