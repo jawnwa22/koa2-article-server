cdf1cae76ecc898ba4c7ba0fdd88b8c4c23a5a08
Jackie Wong <924358746@qq.com>
2019-03-24 22:58:59 +0800

    更新model的导出

--- rest/models/index.js
+++ rest/models/index.js
@@ -1,12 +1,13 @@
 import ArticleModel from './article/article.model';
 import CategoryModel from "./article/category.model";
 import TagModel from './article/tag.model';
-import UserModel from './article/user.model';
-
+import UserModel from './blog/user.model';
+import BlogModel from './blog/blog.model';
 
 export {
   ArticleModel,
   CategoryModel,
   TagModel,
-  UserModel
+  UserModel,
+  BlogModel
 }
