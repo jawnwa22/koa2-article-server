034f4ecde1648980f6c9d017ffca2b568d549c21
Jackie Wong <924358746@qq.com>
2019-03-24 10:01:34 +0800

    修改部分需要权限的URL

--- rest/routes/article/index.js
+++ rest/routes/article/index.js
@@ -8,9 +8,9 @@ import Article from '../../controllers/article';
 const router = new Router();
 
 router
-  .post('/article', Article.upload)                     // 上传文章
-  .put('/article/:id', Article.update)                  // 更新文章
-  .delete('/article/:id', Article.delete)               // 删除文章
+  .post('/admin/article', Article.upload)                     // 上传文章
+  .put('/admin/article/:id', Article.update)                  // 更新文章
+  .delete('/admin/article/:id', Article.delete)               // 删除文章
   .get('/article/list', Article.get_list)               // 获取文章列表
   .get('/article/total_page', Article.get_totoal_page)  // 获取总页数
   .get('/article/:id', Article.get_article)             // 获取文章内容
