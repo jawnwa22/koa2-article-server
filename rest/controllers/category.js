f0d05cb7ab53b6b2a09bbbbabb86a688b6b8bb1b
jawnwa22 <jawnwa22@gmail.com>
2019-08-04 12:12:42 +0800

    refactor(类别控制器)：重构新增类别的控制器

--- rest/controllers/category.js
+++ rest/controllers/category.js
@@ -1,22 +1,38 @@
 /**
- * @description 管理厚爱文章的相关接口逻辑
+ * @description: 文章类别的接口逻辑层
+ * @since: 2019-07-12 20:35:37
+ * @Author: jawnwa22
+ * @LastEditors: jawnwa22
+ * @LastEditTime: 2019-08-04 12:11:53
  */
+
 import { CategoryModel } from "../models/index";
 
 class Category {
-    // 添加类别
+    /**
+     * @description: 添加标签
+     * @param {type} 
+     * query：cate[] {array}
+     * @return: 
+     */
     static async add(ctx) {
-        let cate = ctx.query.cate;
-
-        let isExist = await CategoryModel.findOne({ cate_name: cate });
-
-        let add_cate = async cate => {
-            //添加到categoryModel中
-            await CategoryModel.create({ cate_name: cate });
-        };
-
-        !isExist && add_cate(cate);
-        !isExist ? ctx.success({ msg: "分类添加成功" }) : ctx.error({ msg: "该分类已存在" });
+        let cate  = ctx.query["cate[]"];
+        console.log(cate);
+        
+        let promiseArr = [];
+        cate.forEach(item => {
+            promiseArr[promiseArr.length] = new Promise(resolve => {
+                resolve(CategoryModel.create({
+                    cate_name: item
+                }))
+            })
+        })
+        let arr = await Promise.all(promiseArr);
+        console.log(arr);
+        
+        ctx.success({
+            msg: "添加成功",
+        })
     }
 
     //获取已有的类别
