ec5f20286d80039df396651ed562797e8cd83f09
jawnwa22 <jawnwa22@gmail.com>
2019-08-04 15:07:36 +0800

    feat(类别控制器)：新增类别删除功能

--- rest/controllers/category.js
+++ rest/controllers/category.js
@@ -3,7 +3,7 @@
  * @since: 2019-07-12 20:35:37
  * @Author: jawnwa22
  * @LastEditors: jawnwa22
- * @LastEditTime: 2019-08-04 12:51:12
+ * @LastEditTime: 2019-08-04 15:05:44
  */
 
 import { CategoryModel } from "../models/index";
@@ -11,33 +11,34 @@ import { CategoryModel } from "../models/index";
 class Category {
     /**
      * @description: 添加标签
-     * @param {type} 
+     * @param {type}
      * query：cate[] {array}
-     * @return: 
+     * @return:
      */
     static async add(ctx) {
-        let cate  = ctx.query["cate[]"];
-        
+        let cate = ctx.query["cate[]"];
+
         //判断cate是否为array
         if (cate instanceof Array) {
-            
             let promiseArr = [];
             cate.forEach(item => {
                 promiseArr[promiseArr.length] = new Promise(resolve => {
-                    resolve(CategoryModel.create({
-                        cate_name: item
-                    }))
-                })
-            })
+                    resolve(
+                        CategoryModel.create({
+                            cate_name: item
+                        })
+                    );
+                });
+            });
             await Promise.all(promiseArr);
         } else {
             await CategoryModel.create({
                 cate_name: cate
-            })
+            });
         }
         ctx.success({
-            msg: "添加成功",
-        })
+            msg: "添加成功"
+        });
     }
 
     //获取已有的类别
@@ -48,7 +49,26 @@ class Category {
             data: {
                 result: category
             }
-        })
+        });
+    }
+
+    /**
+     * @description: 删除类别
+     * @param {type}
+     * id: 类别对应的id {String}
+     * @return:
+     */
+    static async deleteCategory(ctx) {
+        let { id } = ctx.query;
+        let isExist = null;
+        if (id) {
+            isExist = await CategoryModel.findByIdAndDelete({ _id: id });
+        } else {
+            ctx.error({ msg: "缺少参数id" });
+            return;
+        }
+
+        isExist ? ctx.success({ msg: "类别删除成功" }) : ctx.error({ msg: "不存在该类别" });
     }
 }
 
--- rest/routes/category/index.js
+++ rest/routes/category/index.js
@@ -1,16 +1,16 @@
 /**
  * @description 管理文章类别的接口配置
- * 
+ *
  */
 
-import Router from 'koa-router';
-import Category from '../../controllers/category';
+import Router from "koa-router";
+import Category from "../../controllers/category";
 
 const router = new Router();
 
 router
-  .post('/category', Category.add)
-  .get('/category', Category.show);
-
+    .post("/category", Category.add)
+    .get("/category", Category.show)
+    .delete("/category", Category.deleteCategory);
 
 export default router;
